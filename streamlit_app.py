0edc28d4753f5e87ff0586938690b9ca65a8564d
Bhavneet Kaur Kaberwal <30475575+Bhavneetkaurkaberwal@users.noreply.github.com>
2023-08-16 12:42:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,5 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import openai
 
 """
 # Welcome to Streamlit!
@@ -16,23 +13,23 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+def main():
+    st.title("FAQ Chatbot")
+    user_question = st.text_input("Ask a question:")
+    
+    if user_question:
+        try:
+            # Call the OpenAI API to generate a response
+            response = openai.Completion.create(
+                engine="davinci",  # You can also use "text-davinci-003"
+                prompt=user_question,
+                max_tokens=50
+            )
+            
+            # Display the generated response to the user
+            st.text("Chatbot: " + response.choices[0].text.strip())
+        except Exception as e:
+            st.error("An error occurred: " + str(e))
 
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if __name__ == "__main__":
+    main()
