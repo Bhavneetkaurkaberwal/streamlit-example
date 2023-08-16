c11a2dd0ec1aeb1a8d9a2a27d5d813ff2d38b644
Bhavneet Kaur Kaberwal <30475575+Bhavneetkaurkaberwal@users.noreply.github.com>
2023-08-16 13:52:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import openai
 
 """
 # Welcome to Streamlit!
@@ -13,23 +12,32 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 
 
-def main():
-    st.title("FAQ Chatbot")
-    user_question = st.text_input("Ask a question:")
-    
-    if user_question:
-        try:
-            # Call the OpenAI API to generate a response
-            response = openai.Completion.create(
-                engine="davinci",  # You can also use "text-davinci-003"
-                prompt=user_question,
-                max_tokens=50
-            )
-            
-            # Display the generated response to the user
-            st.text("Chatbot: " + response.choices[0].text.strip())
-        except Exception as e:
-            st.error("An error occurred: " + str(e))
+class FAQChatbot:
+    def __init__(self):
+        self.faq_database = {
+            "What is ChatGPT Mini?": "ChatGPT Mini is a smaller version of the ChatGPT model designed for quick responses.",
+            "How much does ChatGPT Mini cost?": "ChatGPT Mini is available for free for limited usage. For extended usage, please refer to our pricing page.",
+            "How can I integrate ChatGPT Mini into my website?": "You can integrate ChatGPT Mini using our provided APIs. Documentation is available on our official website.",
+            "Is there customer support for ChatGPT Mini?": "Yes, we offer 24/7 customer support for ChatGPT Mini subscribers.",
+            "Is my data safe with ChatGPT Mini?": "Yes, we prioritize user data privacy and do not store any personal data from user queries."
+        }
 
+    def get_response(self, query):
+        response = self.faq_database.get(query, "Sorry, I don't have an answer for that. Please check our official website or contact support.")
+        return response
+
+    def demo(self):
+        while True:
+            user_input = input("You: ")
+            if user_input.lower() in ['exit', 'quit', 'bye']:
+                print("Chatbot: Goodbye!")
+                break
+            response = self.get_response(user_input)
+            print("Chatbot:", response)
+
+
+# Let's demo the chatbot
 if __name__ == "__main__":
-    main()
+    bot = FAQChatbot()
+    print("ChatGPT Mini FAQ Chatbot! Type 'exit' to quit.")
+    bot.demo()
